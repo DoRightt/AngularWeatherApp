5f9d2ac710fd068a6b3e057bb4c31d44801e1d9d
Sukhov Anton <littlegreenbagg@googlemail.com>
2019-02-09 15:05:35 +0300

    result v1

--- angular-weather-app/src/app/main/current-weather/current-weather.component.ts
+++ angular-weather-app/src/app/main/current-weather/current-weather.component.ts
@@ -10,13 +10,39 @@ import {SimpleChanges} from "@angular/core";
   styleUrls: ['./current-weather.component.scss']
 })
 export class CurrentWeatherComponent implements OnInit {
-  weatherCatalog;
+  weather: Observable<any>;
+  currentWeatherState;
+  temp: number;
+  wind;
+  humidity;
+  pressure;
+  icon;
+  desc;
+  date = Date.now();
 
   constructor(private weatherService: WeatherService) {
+    this.getWeather();
   }
 
   ngOnInit() {
-        this.weatherService.setPosition();
   }
+
+    async getWeather() {
+        try {
+            const position  = await this.weatherService.getCurrentPosition();
+            this.weather = this.weatherService.getWeather(position);
+            this.weather.subscribe(res => {
+              this.currentWeatherState = res.data.current_condition[0]
+              this.temp = this.currentWeatherState.temp_C;
+              this.wind = Math.round(this.currentWeatherState.windspeedKmph / 3.6);
+              this.humidity = this.currentWeatherState.humidity;
+              this.pressure = this.currentWeatherState.pressure;
+              this.icon = this.currentWeatherState.weatherIconUrl[0].value;
+              this.desc = this.currentWeatherState.weatherDesc[0].value;
+            })
+        } catch(error) {
+            this.weather = of(`You'll not get the Geolocation Services to work in this mode. Try Clicking on the 'Open in New Window LIVE' button to see if that works for you!`);
+        }
+    }
 
 }
--- angular-weather-app/src/app/weather.service.ts
+++ angular-weather-app/src/app/weather.service.ts
@@ -1,6 +1,7 @@
+///<reference path="../../node_modules/@angular/common/http/src/params.d.ts"/>
 import { Injectable } from '@angular/core';
 import {Observable, of, Subject} from "rxjs/index";
-import {HttpClient, HttpHeaders } from "@angular/common/http";
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -9,32 +10,22 @@ export class WeatherService {
     key = 'dd6301427900459c863160646190201';
     proxyUrl ='https://cors-anywhere.herokuapp.com/';
     weatherCatalog;
-    city = '';
-    lat = '';
-    lng = '';
-    coords = [];
-    queryString = '';
 
   constructor(private http: HttpClient) { }
 
-  setProps(position) {
-    this.lat = position.coords.latitude;
-    this.lng = position.coords.longitude;
-    this.coords = [position.coords.latitude, position.coords.longitude];
-    this.queryString = `http://api.worldweatheronline.com/premium/v1/weather.ashx?key=${this.key}&q=${position.coords.latitude},${position.coords.longitude}&num_of_days=14&tp=4&format=json`;
+    getWeather(position) {
+        const { latitude, longitude } = position.coords;
+        const queryString = `https://api.worldweatheronline.com/premium/v1/weather.ashx?key=${this.key}&q=${latitude},${longitude}&num_of_days=14&tp=4&format=json`;
+        return this.http.get(queryString);
+    }
 
-    this.getWeather()
-  }
+    async getCurrentPosition() {
+        return new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(
+                res => resolve(res),
+                err => reject(err)
+            );
+        });
+    }
 
-  getWeather() {
-    this.http.get(this.queryString)
-        .subscribe(
-            res => this.weatherCatalog = res,
-            err => console.error(err)
-        );
-  }
-
-  setPosition() {
-    navigator.geolocation.getCurrentPosition(this.setProps.bind(this))
-  }
 }
